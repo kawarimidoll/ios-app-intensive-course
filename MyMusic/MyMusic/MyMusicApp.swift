9ce4f35beb954e9fc95ddb7e1d70234c0c08192d
kawarimidoll <kawarimidoll+git@gmail.com>
2021-04-29 11:18:41 +0900

    create MyMusic project

---
+++ MyMusic/MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by kawarimidoll on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
