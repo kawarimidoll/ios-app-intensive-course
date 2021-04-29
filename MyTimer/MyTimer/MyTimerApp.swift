fc790c83f0dd915a4f1449b5bc207d639fdfa8cb
kawarimidoll <kawarimidoll+git@gmail.com>
2021-04-29 18:25:21 +0900

    create MyTimer project

---
+++ MyTimer/MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by kawarimidoll on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
