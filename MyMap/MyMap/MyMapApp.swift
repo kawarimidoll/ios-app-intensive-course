50635533bd1e8cde97db65f9b02e5fe87857ce6e
kawarimidoll <kawarimidoll+git@gmail.com>
2021-04-29 15:35:53 +0900

    create MyMap project

---
+++ MyMap/MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by kawarimidoll on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
