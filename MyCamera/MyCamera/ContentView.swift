25911a11b1718f119c99cf6af5f170f600dc75b2
kawarimidoll <kawarimidoll+git@gmail.com>
2021-04-30 14:19:18 +0900

    add camera feature

--- MyCamera/MyCamera/ContentView.swift
+++ MyCamera/MyCamera/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State var captureImage: UIImage? = nil
+    @State var isShowSheet = false
+
     var body: some View {
-        Text("Hello, world!")
+
+        VStack {
+            Spacer()
+
+            if let unwrapCpautreImage = captureImage {
+                Image(uiImage: unwrapCpautreImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+
+            Spacer()
+
+            Button(action: {
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    print("available")
+                    isShowSheet = true
+                } else {
+                    print("disabled")
+                }
+            }) {
+                Text("Take a photo")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+            }
             .padding()
+            .sheet(isPresented: $isShowSheet) {
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
+        }
     }
 }
 
